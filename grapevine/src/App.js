534604ddb28ab5eee550771f651172647d87ced3
Tian3401 <matt.chen3401@yahoo.com>
2020-03-30 21:57:34 -0700

    update navbar

--- grapevine/src/App.js
+++ grapevine/src/App.js
@@ -4,30 +4,44 @@ import Navbar from './components/Navbar'
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
 } from 'react-router-dom'; 
 
-function App() {
+export default function App() {
   return (
     <div class="App">
       <Router>
         <Navbar/>
         <Switch>
-          <Route path="/">
-          </Route>
-          <Route path="/share">
-            <Share/>
-          </Route>
+          <CreateRoute exact path="/" components={Home}/>
+          <CreateRoute path="/share" components={Share}/>
+          <CreateRoute path="/explore" components={Explore}/>
         </Switch>
       </Router>
-      
     </div>
   );
+}
+
+function Home() {
+  return(
+    <div></div>
+  )
+}
+
+function CreateRoute(props) {
+  return(
+    <Route
+      path={props.path}
+    >
+      {props.components}
+    </Route>
+  )
 }
 
 function Share() {
   return <h2>Share</h2>
   
 }
-
-export default App;
+function Explore() {
+  return <h2>Explore</h2>
+}
--- grapevine/src/components/Navbar.js
+++ grapevine/src/components/Navbar.js
@@ -8,16 +8,20 @@ export default function Navbar() {
   return (
     < nav class="navbar">
       <ul>
-        <li class="nav-link" id="logo">Grapevine</li>
+        <li class="nav-link" id="logo">
+          <Link to="/" class="link">
+            Grapevine
+          </Link>
+        </li>
       </ul>
       <ul class="extra-links">
         <li class="nav-link">
-          <Link to="/share">
+          <Link to="/share" class ="link">
             Share
           </Link>
         </li>
         <li class="nav-link">
-          <Link to="/explore">
+          <Link to="/explore" class ="link">
             Explore
           </Link>
         </li>
