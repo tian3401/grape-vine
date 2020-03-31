73ebae49a466013774ad1011542e6f09e56aca37
Tian3401 <matt.chen3401@yahoo.com>
2020-03-31 09:19:49 -0700

    create vine logo and animations

--- grapevine/src/components/Navbar.js
+++ grapevine/src/components/Navbar.js
@@ -8,10 +8,22 @@ export default function Navbar() {
   return (
     < nav class="navbar">
       <ul>
-        <li class="nav-link" id="logo">
+        <li class="nav-link" id="name">
           <Link to="/" class="link">
             Grapevine
           </Link>
+        </li>
+      </ul>
+      <ul class="vine-container">
+        <li class="vine-logo">
+          <div class="dot"></div>
+          <div class="dot"></div>
+          <div class="dot"></div>
+          <div class="vine">
+          </div>
+          <div class="dot"></div>
+          <div class="dot"></div>
+          <div class="dot"></div>
         </li>
       </ul>
       <ul class="extra-links">
