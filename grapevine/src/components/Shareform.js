c5630f8986c065af1b6821cd0adf553bdd437acc
Tian3401 <matt.chen3401@yahoo.com>
2020-04-01 10:00:05 -0700

    animate shareform

--- grapevine/src/components/Shareform.js
+++ grapevine/src/components/Shareform.js
@@ -9,7 +9,8 @@ class Login extends React.Component {
       email: "",
       dataLabel: "",
       dataPoint: "",
-      location: ""
+      location: "",
+      classValue: "grape"
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,23 +26,31 @@ class Login extends React.Component {
   }
 
   handleSubmit(event) {
-    console.log(`submitted form with following details => name: ${this.state.name} email: ${this.state.email} data label: ${this.state.dataLabel} data point: ${this.state.dataPoint} location: ${this.state.location} `)
     event.preventDefault();
+    this.toggleClass(); 
+    console.log(`submitted form with following details => name: ${this.state.name} email: ${this.state.email} data label: ${this.state.dataLabel} data point: ${this.state.dataPoint} location: ${this.state.location} `)
+  }
+
+  toggleClass() {
+    let element = (this.state.classValue === "grape") ? "clicked-grape" : "grape";
+    this.setState({
+      "classValue": element
+    });
   }
 
   render() {
     return(
       <form onSubmit={this.handleSubmit} className="form">
         <label>name:</label>
-        <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/>
+        <input type="text" name="name" className={this.state.classValue} value={this.state.name} onChange={this.handleChange}/>
         <label>email:</label>
-        <input type="text" name="email" value={this.state.email} onChange={this.handleChange}/>
+        <input type="text" name="email" className={this.state.classValue}  value={this.state.email} onChange={this.handleChange}/>
         <label>data label:</label>
-        <input type="text" name="dataLabel" value={this.state.dataLabel} onChange={this.handleChange}/>
+        <input type="text" name="dataLabel"  className={this.state.classValue} value={this.state.dataLabel} onChange={this.handleChange}/>
         <label>data point:</label>
-        <input type="text" name="dataPoint" value={this.state.dataPoint} onChange={this.handleChange}/>
+        <input type="text" name="dataPoint" className={this.state.classValue}  value={this.state.dataPoint} onChange={this.handleChange}/>
         <label>location:</label>
-        <input type="text" name="location" value={this.state.location} onChange={this.handleChange}/>
+        <input type="text" name="location" className={this.state.classValue}  value={this.state.location} onChange={this.handleChange}/>
         <input type="submit" value="Submit"/>
       </form>
     )
