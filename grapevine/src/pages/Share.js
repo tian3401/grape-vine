bd0f95db14580f1576fbe9804d6de2ba255d7544
Tian3401 <matt.chen3401@yahoo.com>
2020-04-02 09:22:37 -0700

    add pages folder

---
+++ grapevine/src/pages/Share.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+import Shareform from '../components/Shareform'
+
+const Share = () => {
+  return (
+  <div>
+    <h1 className="title">Thanks for Sharing</h1>
+    <Shareform/>
+  </div>
+  )
+}
+
+export default Share; 
