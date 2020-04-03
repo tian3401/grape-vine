85697f73ee688beb78f69886a19227e64a85d6fa
Tian3401 <matt.chen3401@yahoo.com>
2020-04-02 19:08:57 -0700

    implement dynamic assignment of avatars to boxes

--- grapevine/src/App.js
+++ grapevine/src/App.js
@@ -45,38 +45,37 @@ function Home() {
   )
 }
 
-function Box(props) {
+const Box = (props) => {
   return (
     <div className="box" id={props.position}>
-      {props.value}
+      <img className="avatar-img" alt="avatar" src={require('./assets/avatars/avatar_sample_'+ props.id + '.png')}/>
     </div>
   );
-
 }
 
 class Matrix extends React.Component {
   renderBox(data) {
     return(
-      <Box position={data}/>
+      <Box position={data.position} id={data.id}/>
     );
   }
   render() {
     return(
       <div>
         <div className="matrix-row">
-          {this.renderBox("top-left")}
-          {this.renderBox("top-center")}
-          {this.renderBox("top-right")}
+          {this.renderBox({position: "top-left", id: 1})}
+          {this.renderBox({position: "top-center", id: 2})}
+          {this.renderBox({position: "top-right", id: 3})}
         </div>
         <div className="matrix-row">
-          {this.renderBox("middle-left")}
-          {this.renderBox("middle-center")}
-          {this.renderBox("middle-right")}
+          {this.renderBox({position: "middle-left", id: 4})}
+          {this.renderBox({position: "middle-center", id: 5})}
+          {this.renderBox({position: "middle-right", id: 6})}
         </div>
         <div className="matrix-row">
-          {this.renderBox("bottom-left")}
-          {this.renderBox("bottom-center")}
-          {this.renderBox("bottom-right")}
+          {this.renderBox({position: "bottom-left", id: 7})}
+          {this.renderBox({position: "bottom-center", id: 8})}
+          {this.renderBox({position: "bottom-right", id: 9})}
         </div>
       </div>
     )
