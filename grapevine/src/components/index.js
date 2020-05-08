4419589e19a8448fac205cf0cf29e6e3cc768dbf
Tian3401 <matt.chen3401@yahoo.com>
2020-05-07 20:14:53 -0700

    create Context module and export firebase folder content via index.js

---
+++ grapevine/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Matrix } from './Matrix'
+export { default as Navbar } from './Navbar'
+export { default as CreateRoute } from './Routes'
+export { default as Shareform } from './Shareform'
+export { default as Signupform } from './Signupform'
---
+++ grapevine/src/firebase/index.js
@@ -0,0 +1,6 @@
+import FirebaseContext from './Context';
+import FireApp from '../config/firebase';
+
+export default FireApp; 
+
+export { FirebaseContext }; 
